50c30943154cb69922ef1f1eccf0fb33eacde08f
Pounct <53088375+pounct@users.noreply.github.com>
2023-08-21 09:00:51 +0200

    Update ServeisORM.java

--- src/nivell2/ServeisORM.java
+++ src/nivell2/ServeisORM.java
@@ -99,90 +99,86 @@ public class ServeisORM {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addCompra(Compra c) {
 
 		Connection conn = connexio();
 		String peticio = "INSERT INTO compra (dateC) VALUES (?);DECLARE @Id INT = SCOPE_IDENTITY();";
 		try {
 			PreparedStatement ps = conn.prepareStatement("INSERT INTO compra (dateC) VALUES (?);",
-                    Statement.RETURN_GENERATED_KEYS);
-			
+					Statement.RETURN_GENERATED_KEYS);
+
 			ps.setDate(1, c.getDate());
 			int rs = ps.executeUpdate();
-			
+
 			if (rs == 0) {
-	            throw new SQLException("Creating compra failed, no rows affected.");
-	        }else{
-	        	try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
-	                if (generatedKeys.next()) {
-	                    c.setId(generatedKeys.getInt(1));
-	                }
-	                else {
-	                    throw new SQLException("Creating user failed, no ID obtained.");
-	                }
-	            }
-	        	
-	        }
+				throw new SQLException("Creating compra failed, no rows affected.");
+			} else {
+				try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						c.setId(generatedKeys.getInt(1));
+					} else {
+						throw new SQLException("Creating user failed, no ID obtained.");
+					}
+				}
+
+			}
 
 		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
 	}
+
 	public void addProducte(Producte p) {
 
 		Connection conn = connexio();
-		
+
 		String peticioArbre = "INSERT INTO arbre (id, alcada) VALUES (?,?);";
 		String peticioFlor = "INSERT INTO flor (id, color) VALUES (?,?);";
 		String peticioDecoracio = "INSERT INTO decoracio (id, designacio) VALUES (?,?);";
-		
-		//String peticio = "INSERT INTO producte (designacio) VALUES (?);";
-		
+
+		// String peticio = "INSERT INTO producte (designacio) VALUES (?);";
+
 		if (p.getClass().getName().equals("entitats.Arbre")) {
-			
-			
+
 			try {
 				System.out.println("Producte");
 				PreparedStatement ps = conn.prepareStatement("INSERT INTO producte (designacio) VALUES (?);",
-	                    Statement.RETURN_GENERATED_KEYS);
+						Statement.RETURN_GENERATED_KEYS);
 				ps.setString(1, p.getDesignacio());
 				int rs = ps.executeUpdate();
-				
+
 				if (rs == 0) {
-		            throw new SQLException("Creating compra failed, no rows affected.");
-		        }else{
-		        	try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
-		                if (generatedKeys.next()) {
-		                    p.setId(generatedKeys.getInt(1));
-		                }
-		                else {
-		                    throw new SQLException("Creating user failed, no ID obtained.");
-		                }
-		            }
-		        	
-		        }
+					throw new SQLException("Creating compra failed, no rows affected.");
+				} else {
+					try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+						if (generatedKeys.next()) {
+							p.setId(generatedKeys.getInt(1));
+						} else {
+							throw new SQLException("Creating user failed, no ID obtained.");
+						}
+					}
+
+				}
 				System.out.println("Arbre");
 				PreparedStatement ps3 = conn.prepareStatement("INSERT  INTO arbre (id, alcada) VALUES (?,?);");
 				ps3.setInt(1, p.getId());
 				ps3.setDouble(2, ((Arbre) p).getAlcada());
 				ps3.executeUpdate();
 			} catch (SQLException e) {
-				
+
 				e.printStackTrace();
 			}
-			
-			
-			
-		} else if(p.getClass().getName().equals("entitats.Flor")) {
+
+		} else if (p.getClass().getName().equals("entitats.Flor")) {
 			System.out.println("Flor");
 			try {
 				System.out.println("Producte");
 				PreparedStatement ps = conn.prepareStatement("INSERT INTO producte (designacio) VALUES (?);");
 				ps.setString(1, p.getDesignacio());
 				ps.executeUpdate();
-				PreparedStatement ps2= conn.prepareStatement("SELECT * FROM producte WHERE designacio like ?;");
+				PreparedStatement ps2 = conn.prepareStatement("SELECT * FROM producte WHERE designacio like ?;");
 				ps2.setString(1, p.getDesignacio());
 				ResultSet rs = ps2.executeQuery();
 				while (rs.next()) {
@@ -194,17 +190,45 @@ public class ServeisORM {
 				ps3.setString(2, ((Flor) p).getColor());
 				ps3.executeUpdate();
 			} catch (SQLException e) {
-				
+
 				e.printStackTrace();
 			}
-		} else if(p.getClass().getName().equals("entitats.Decoracio")) {
+		} else if (p.getClass().getName().equals("entitats.Decoracio")) {
 			System.out.println("Decoracio");
-		}
-		else {
-			System.out.println("Nada" +p.getClass().getName());
+			try {
+				System.out.println("Producte");
+				PreparedStatement ps = conn.prepareStatement("INSERT INTO producte (designacio) VALUES (?);",
+						Statement.RETURN_GENERATED_KEYS);
+				ps.setString(1, p.getDesignacio());
+				int rs = ps.executeUpdate();
+
+				if (rs == 0) {
+					throw new SQLException("Creating compra failed, no rows affected.");
+				} else {
+					try (ResultSet generatedKey = ps.getGeneratedKeys()) {
+						if (generatedKey.next()) {
+							p.setId(generatedKey.getInt(1));
+						} else {
+							throw new SQLException("Creating user failed, no ID obtained.");
+						}
+					}
+
+				}
+				System.out.println("Decoracio");
+				PreparedStatement ps3 = conn.prepareStatement("INSERT  INTO decoracio (id, material) VALUES (?,?);");
+				ps3.setInt(1, p.getId());
+				ps3.setString(2, ((Decoracio) p).getMaterial().toString());
+				ps3.executeUpdate();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Nada" + p.getClass().getName());
 		}
 	}
-	public void addLineaCompra(Compra c,Producte p,double preu) {
+
+	public void addLineaCompra(Compra c, Producte p, double preu) {
 
 		Connection conn = connexio();
 		String peticio = "INSERT INTO liniacompra (preu,floresteriaId,producteId, compraId) VALUES (?,1,?,?);";
